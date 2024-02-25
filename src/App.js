f9b210eba9e017afcfb00bda6cd11342ea7e4a10
Danny Holman <dannygholman@gmail.com>
2024-02-25 16:07:21 -0700

    Header Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Nav from './Components/Nav'
+import Header from './Components/Header';
 
 function App() {
   return (
     <>
     <Nav />
+    <Header />
     </>
   );
 }
---
+++ src/Components/BookingForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BookingForm() {
+  return (
+    <div>
+      Booking Form
+    </div>
+  );
+}
+
+export default BookingForm;
